037597a1ec58b9d929fc319de3c86c68b3f08ed1
Marcos Traverso <marcostraverso2003@gmail.com>
2024-04-03 02:29:21 -0400

    Add CLI handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+use clap::Parser;
+
+
+
+/// A simple program to interpret Brainfuck programs.
+#[derive(Parser)]
+#[command(about, version)]
+struct Cli {
+    /// A Brainfuck program to interpret
+    #[arg(short, long)]
+    input: String,
+    /// An optional file to output to. If not specified, the program uses stdout.
+    #[arg(short, long)]
+    output: Option<String>,
+    /// Enables or disables wrapping of the data pointer.
+    #[arg(short, long)]
+    wrapping: bool,
+    /// The size of the memory tape.
+    #[arg(short, long, default_value_t = 30000)]
+    size: usize,
+    /// Enable debug information. This will print the memory tape at each instruction.
+    #[arg(short, long)]
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello World!")
+    let args: Cli = Cli::parse();
 }
